bdaa349865eb4ba62f134396a9ffb374424003bc
iamali123 <iamali62426@gmail.com>
2023-02-02 21:01:35 +0400

    project setup

---
+++ shareme_backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'i2u5vzz1',
+    dataset: 'production'
+  }
+})
---
+++ shareme_frontend/shareme_react/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GoogleLogin from 'react-google-login'
+
+const Login = () => {
+  return (
+    <div>
+        <GoogleLogin />
+    </div>
+  )
+}
+
+export default Login
