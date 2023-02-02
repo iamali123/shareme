5caf82f31c4c88a1fe572c69ee8c9b9682303d40
iamali123 <iamali62426@gmail.com>
2023-02-02 21:06:22 +0400

    Google button added

--- shareme_frontend/shareme_react/src/components/Login.jsx
+++ shareme_frontend/shareme_react/src/components/Login.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import GoogleLogin from 'react-google-login'
+import GoogleLogin from 'react-google-login';
+import { FcGoogle } from 'react-icons/fc';
 
 const Login = () => {
   return (
     <div>
-        <GoogleLogin />
+         <GoogleLogin
+              clientId=''
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4" /> Sign in with google
+                </button>
+              )}
+            //   onSuccess={responseGoogle}
+            //   onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+            />
     </div>
   )
 }
